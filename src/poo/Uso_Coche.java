8350a9bec73952d1db4995ea7050faef410cc18d
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-03-27 23:08:55 -0300

    oop 2

---
+++ src/poo/Uso_Coche.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Coche renault = new Coche();
+
+		System.out.println("Este coche tiene " + renault.ruedas + " ruedas");
+	}
+
+}
