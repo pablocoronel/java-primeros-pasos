3d0881e307660e988ea8ff812638a3f55e9bd2ee
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-03-31 00:48:18 -0300

    oop 8

--- src/poo/Uso_Empleado.java
+++ src/poo/Uso_Empleado.java
@@ -1,6 +1,7 @@
 package poo;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Uso_Empleado {
 
@@ -15,9 +16,32 @@ class Empleado {
 	private String nombre;
 	private double sueldo;
 	private Date altaContrato;
-	
-	public Empleado(String nom, double sue, int anho, int mes, int dia) {
 
-		
+	// constructor
+	public Empleado(String nom, double sue, int anho, int mes, int dia) {
+		this.nombre = nom;
+		this.sueldo = sue;
+
+		GregorianCalendar calendario = new GregorianCalendar(anho, mes - 1, dia);
+		this.altaContrato = calendario.getTime();
+	}
+
+	// getter
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public double getSueldo() {
+		return this.sueldo;
+	}
+
+	public Date getAltaContrato() {
+		return this.altaContrato;
+	}
+
+	// metodos
+	public void aumentarSueldo(double porcentaje) {
+		double aumento = this.sueldo * (porcentaje / 100);
+		this.sueldo += aumento;
 	}
 }
