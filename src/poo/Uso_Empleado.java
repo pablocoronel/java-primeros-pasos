33de4f0ba54266c9144c86b8249560a7067a5053
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-03-31 00:02:06 -0300

    oop 7

---
+++ src/poo/Uso_Empleado.java
@@ -0,0 +1,23 @@
+package poo;
+
+import java.util.Date;
+
+public class Uso_Empleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Empleado {
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
+	
+	public Empleado(String nom, double sue, int anho, int mes, int dia) {
+
+		
+	}
+}
