f6d855c59f79f821f8fe4e30cfcbb776f8a6b4da
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-03-31 18:23:37 -0300

    atributos static

--- src/poo/Pruebas_final.java
+++ src/poo/Pruebas_final.java
@@ -7,24 +7,36 @@ public class Pruebas_final {
 
 		Empleados trabajador1 = new Empleados("paco");
 		Empleados trabajador2 = new Empleados("cacho");
+		Empleados trabajador3 = new Empleados("maria");
+		Empleados trabajador4 = new Empleados("lisandro");
 
 		trabajador1.setSeccion("rr.hh.");
 
 		System.out.println(trabajador1.getDatos());
+
+		// incrementa el atributo estatico
+//		Empleados.id++; // se incrementa antes de la 2da instancia, pero no es automatico 
 		System.out.println(trabajador2.getDatos());
+
+		System.out.println(trabajador3.getDatos());
+		System.out.println(trabajador4.getDatos());
 	}
 
 }
 
 //no tiene que ser publica
 class Empleados {
-	private final String nombre; //con final, es una constante y no se puede modificar
+	private final String nombre; // con final, es una constante y no se puede modificar
 	private String seccion;
+	private int id; // en cursiva aparece para eclipse los atributos estaticos
+	private static int idSiguiente = 1;
 
 	// constructor
 	public Empleados(String nom) {
 		this.nombre = nom;
 		this.seccion = "administracion";
+		this.id = this.idSiguiente;
+		this.idSiguiente++;
 	}
 
 	// setter
@@ -32,13 +44,13 @@ class Empleados {
 		this.seccion = seccion;
 	}
 
-	///esto no se puede hacer, xq this.nombre es una constante (con final)
+	/// esto no se puede hacer, xq this.nombre es una constante (con final)
 //	public void setNombre(String nombre) {
 //		this.nombre = nombre;
 //	}
 
 	// getter
 	public String getDatos() {
-		return "El nombre es: " + this.nombre + " y la seccion es: " + this.seccion;
+		return "El nombre es: " + this.nombre + " y la seccion es: " + this.seccion + " el ID es: " + this.id;
 	}
 }
