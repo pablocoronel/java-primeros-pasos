c3e09554b43073a5b5203a347a32e985463f2bf4
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-04-06 23:00:38 -0300

    interfaces 4, temporizador

---
+++ src/poo/Prueba_Temporizador.java
@@ -0,0 +1,43 @@
+package poo;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+
+public class Prueba_Temporizador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// instancia de una clase que implementa la interface ActionListener
+		DameLaHora oyente = new DameLaHora();
+
+//		ActionListener oyente = new DameLaHora(); esto tambien funciona
+		
+		// crea un temporizador
+		Timer mi_temporizador = new Timer(5000, oyente);
+
+		// comienza
+		mi_temporizador.start();
+
+		// para dejar el programa en ejecucion asi se ve
+		JOptionPane.showMessageDialog(null, "pulsa para detener");
+	}
+
+}
+
+// clase que se usa como parametro en el temporizador de la clase Prueba_Temporizador
+class DameLaHora implements ActionListener {
+
+	// metodo que hay que implementar de la interface
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Date ahora = new Date(); // hora actual
+
+		System.out.println("Te pongo la hora cada 5 segundos: " + ahora);
+	}
+
+}
